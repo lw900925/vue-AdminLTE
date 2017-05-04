5bef634e633e8d087e48d721395a13e5a0816a5f
lw900925 <lw900925@163.com>
2017-05-04 11:03:03 +0800

    修改登录页面样式

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,48 +1,45 @@
 <template>
   <div class="login-box">
     <div class="login-logo">
-      <a href="../../index2.html"><b>Admin</b>LTE</a>
+      <a href="javascript: void(0)"><b>Admin</b>LTE</a>
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+      <p class="login-box-msg">登录后开始会话</p>
 
-      <form action="../../index2.html" method="post">
+      <form action="javascript: void(0)" method="post">
         <div class="form-group has-feedback">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="email" class="form-control" placeholder="电子邮箱">
           <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
         </div>
         <div class="form-group has-feedback">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" class="form-control" placeholder="密码">
           <span class="glyphicon glyphicon-lock form-control-feedback"></span>
         </div>
         <div class="row">
           <div class="col-xs-8">
-            <div class="checkbox icheck">
-              <label>
-                <input type="checkbox"> Remember Me
-              </label>
-            </div>
+            <label>
+              <input type="checkbox"> 记住我
+            </label>
           </div>
           <!-- /.col -->
           <div class="col-xs-4">
-            <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+            <button type="submit" class="btn btn-primary btn-block btn-flat">登录</button>
           </div>
           <!-- /.col -->
         </div>
       </form>
 
       <div class="social-auth-links text-center">
-        <p>- OR -</p>
-        <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-          Facebook</a>
-        <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-          Google+</a>
+        <a href="javascript: void(0)" class="btn btn-block btn-social btn-linkedin btn-flat"><i
+          class="fa fa-linkedin"></i> 使用LinkedIn登陆</a>
+        <a href="javascript: void(0)" class="btn btn-block btn-social btn-github btn-flat"><i class="fa fa-github"></i>
+          使用GitHub登陆</a>
       </div>
       <!-- /.social-auth-links -->
 
-      <a href="#">I forgot my password</a><br>
-      <a href="register.html" class="text-center">Register a new membership</a>
+      <a href="javascript: void(0)">忘记密码</a><br>
+      <a href="javascript: void(0)" class="text-center">注册新用户</a>
 
     </div>
     <!-- /.login-box-body -->
@@ -50,6 +47,19 @@
 </template>
 
 <script>
+  export default {
+    name: 'login',
+    mounted: function () {
+      console.log($('input[type="checkbox"]').html())
+    }
+  }
 
+  /*$(document).ready(function () {
+    $('input[type="checkbox"]').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
+    });
+  })*/
 
 </script>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import './plugins/bootstrap-3.3.6/css/bootstrap.min.css'
-import './plugins/font-awesome-4.6.3/css/font-awesome.min.css'
-import './plugins/ionicons-2.0.0/css/ionicons.min.css'
-import './plugins/AdminLTE-2.3.11/css/AdminLTE.min.css'
-import './plugins/AdminLTE-2.3.11/css/skins/_all-skins.min.css'
+import '@/plugins/bootstrap-3.3.6/css/bootstrap.min.css'
+import '@/plugins/font-awesome-4.6.3/css/font-awesome.min.css'
+import '@/plugins/ionicons-2.0.0/css/ionicons.min.css'
+import '@/plugins/AdminLTE-2.3.11/css/AdminLTE.min.css'
+import '@/plugins/AdminLTE-2.3.11/css/skins/_all-skins.min.css'
+import '@/plugins/iCheck-1.0.1/all.css'
 
-import './plugins/jQuery-2.2.3/jquery.min'
-import './plugins/bootstrap-3.3.6/js/bootstrap.min'
-import './plugins/AdminLTE-2.3.11/js/app.min'
+import '@/plugins/jQuery-2.2.3/jquery.min'
+import '@/plugins/bootstrap-3.3.6/js/bootstrap.min'
+import '@/plugins/AdminLTE-2.3.11/js/app.min'
+import '@/plugins/iCheck-1.0.1/icheck.min'
 
 import Vue from 'vue'
 import App from './App'
